a79da07f381a9fca2d4b9d512cde794151b319af
Alex Smith <alexsmith716@outlook.com>
2017-08-25 22:20:38 -0400

    Added additional dependencies modified server dir for express and mongodb and mongoose. Further researched jwt, jwt authentication and sessions.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-import express from 'express';
 
+import 'dotenv/config';
+
+import express from 'express';
+import helmet from 'helmet';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -10,3 +13,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'static')));
+
+
+export { app };
+
+/*
+app.set('port', process.env.PORT || 3000)
+
+var server = http.createServer(app).listen(3000, '127.0.0.1', function () {
+  console.log('Express server listening on port ' + server.address().port)
+  console.log('Express server listening on port ' + server.address().address)
+})
+*/
